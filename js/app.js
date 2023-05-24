2faf767d9195e4ed7404660049da06a0d6601fee
KrystianGH2 <kgcdronio@gmail.com>
2023-05-24 03:59:02 +0200

    Added menu/blog cards functionalities

--- js/app.js
+++ js/app.js
@@ -1,38 +1,18 @@
-
-const navLinks = document.querySelector(".nav-links");
-const menuButton = document.querySelector(".menuButton");
-const navLinksLi = document.querySelectorAll(".nav-links li");
-
-const navFunction = () => {
-  menuButton.onclick = () => {
-    navLinks.classList.toggle("active");
-    
-    navLinksLi.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.12
-        }s`;
-      }
-    });
-    // menu Animation
-    menuButton.classList.toggle("toggle");
-  };
-};
-
+import{
+  navFunction,
+} from './navigation.js'
 navFunction();
-
 
 // const carouselContainer = document.querySelector(".carousel-container");
 const slider = document.querySelector(".slider");
 const prevButton = document.querySelector("#prev-button");
 const nextButton = document.querySelector("#next-button");
-
+const trendingPostsElements = document.querySelector(".trendingPostsElements");
+const latestFromElements = document.querySelector(".latestFromElements");
 
 const options = { method: "GET" };
 let url =
-"https://marvelous-jargon.flywheelsites.com/wp-json/wp/v2/posts?_embed&per_page=100";
+  "https://marvelous-jargon.flywheelsites.com/wp-json/wp/v2/posts?_embed&per_page=100";
 
 const getBlogs = async () => {
   const getCategoryName = async (categoryId) => {
@@ -41,94 +21,107 @@ const getBlogs = async () => {
     const categoryResult = await categoryData.json();
     return categoryResult.name;
   };
-  
+
   try {
     const data = await fetch(url, options);
     const result = await data.json();
-    
+
     console.log(result);
-    
+
     const blogsItems = result;
-    
+
     for (let i = 0; i < blogsItems.length; i++) {
       const featuredImage =
-      blogsItems[i]._embedded["wp:featuredmedia"][0].media_details.sizes
-      .medium.source_url;
-      
+        blogsItems[i]._embedded["wp:featuredmedia"][0].media_details.sizes
+          .medium.source_url;
+
       const altText = blogsItems[i]._embedded["wp:featuredmedia"][0].alt_text;
-      
-      const categoryId = blogsItems[i].categories[0];
-      
-      
-      
+
+      const categoryId = blogsItems[i].categories[0 && 1 && 2];
+
       const categoryName = await getCategoryName(categoryId);
-      if(categoryName.startsWith("L")) {
+      if (categoryName.startsWith("L")) {
         slider.innerHTML += `
         
         <div class="cards">
         <img class="cardsImg" src="${featuredImage}" alt="${altText}">
+        <a href="details.html?=id">
+            <h5 class="blogTitle">${blogsItems[i].title.rendered}</h5>
+            <div class="smallTags">
+            <p>${blogsItems[i].excerpt.rendered}</p>
+            <small>${categoryName}</small>
+            </div></a>
+        </div>
+        
+        `;
+      } else if (i <= 10) {
+        trendingPostsElements.innerHTML += "";
+
+        trendingPostsElements.innerHTML += `
+        <div class="cards trendingPosts">
+        <img class="cardsImg" src="${featuredImage}" alt="${altText}">
+        <a href="details.html?=id">
         <h5 class="blogTitle">${blogsItems[i].title.rendered}</h5>
         <div class="smallTags">
         <p>${blogsItems[i].excerpt.rendered}</p>
         <small>${categoryName}</small>
-        </div>
-        </div>
-        
-        `;
-        
+        </div></a>
+        </div>`;
+      } else if (
+        categoryName.startsWith("E") &&
+        latestFromElements.children.length < 6
+      ) {
+        latestFromElements.innerHTML += `
+        <div class="cards latestFrom">
+        <img class="cardsImg" src="${featuredImage}" alt="${altText}">
+        <a href="details.html?=id">
+        <h5 class="blogTitle">${blogsItems[i].title.rendered}</h5>
+        <div class="smallTags">
+        <p>${blogsItems[i].excerpt.rendered}</p>
+        <small>${categoryName}</small>
+        </div></a>
+        </div>`;
       }
     }
 
-    const cards = document.querySelector('.cards');
     const carouselItems = document.querySelectorAll(".carouselItems");
+    // const carouselContainer = document.querySelector(".carousel-container");
     let currentIndex = 0;
     const totalItems = carouselItems.length;
     console.log(totalItems);
 
-    // Move to next slide
-    
-    const nextSlide = () =>{
-      currentIndex = currentIndex + 1 * totalItems;
-      if (currentIndex === 0) {
-        currentIndex = totalItems -1 * 0}
+    let carouselItemsWidth = 16.8;
 
-      updateCarousel();
-      
-    }
-    
-    // Move to previous slide
-    
-    const previousSlide = () =>{
-        if(currentIndex <= 0) {
-          currentIndex = currentIndex - 0 * totalItems;
-        } 
-        currentIndex = currentIndex - 1 * totalItems;
-      
-      updateCarousel();
-    }
-    
-    const updateCarousel = () =>{
-      const offset = -currentIndex * 16.8;
+    // Move to next slide
+    const nextSlide = () => {
+      currentIndex = currentIndex + 1 * totalItems;
+      let offset = -currentIndex * carouselItemsWidth;
       slider.style.transform = `translateX(${offset}%)`;
-    }
-    
-    nextButton.addEventListener("click", ()=>{
+    };
+
+    // Move to previous slide
+    const previousSlide = () => {
+      if(offset=== 0) {
+        offset++;
+        prevButton.disabled=true;
+      }
+      currentIndex = currentIndex - 1 * totalItems;
+      let offset = -currentIndex * carouselItemsWidth;
+      slider.style.transform = `translateX(${offset}%)`;
+    };
+
+    nextButton.addEventListener("click", () => {
       console.log("cliked");
       nextSlide();
     });
-    
 
-    prevButton.addEventListener("click", ()=>{
+    prevButton.addEventListener("click", () => {
       console.log("cliked");
       previousSlide();
     });
-    
-    
-
   } catch (e) {
     console.log(e);
   }
 };
 
 getBlogs();
-
---
+++ js/blogs.js
@@ -0,0 +1,162 @@
+import { navFunction } from "./navigation.js";
+navFunction();
+
+const travelBlogs = document.querySelector(".travelBlogsElements");
+
+// --------------PLACES LIST --------------------
+const liAfrica = document.querySelector(".africa-li");
+const liAmericas = document.querySelector(".americas-li");
+const liAntarctica = document.querySelector(".antartica-li");
+const liAsia = document.querySelector(".asia-li");
+const liEurope = document.querySelector(".europe-li");
+const liOceania = document.querySelector(".oceania-li");
+const selectOption = document.querySelector("#selectOption");
+
+const searchEl = document.querySelector(".search");
+
+searchEl.addEventListener("input", async (e) => {
+  setTimeout(async () => {
+    e.preventDefault();
+    clearTravelBlogs();
+    const searchValue = e.target.value.trim().toLowerCase();
+    await getBlogs(searchValue);
+  }, 600);
+});
+
+const options = { method: "GET" };
+let url =
+  "https://marvelous-jargon.flywheelsites.com/wp-json/wp/v2/posts?_embed&per_page=10";
+
+let blogsPostPerPage = 10;
+let selectedValues = "AZ";
+let isLoaded = false;
+
+selectOption.addEventListener("change", async (e) => {
+  selectedValues = e.target.value;
+  clearTravelBlogs();
+  setTimeout(() => {
+    getBlogs();
+  }, 800);
+});
+
+const getBlogs = async (searchValue = "") => {
+  const getCategoryName = async (categoryId) => {
+    const categoryUrl = `https://marvelous-jargon.flywheelsites.com/wp-json/wp/v2/categories/${categoryId}`;
+    const categoryData = await fetch(categoryUrl, options);
+    const categoryResult = await categoryData.json();
+    return categoryResult.name;
+  };
+
+  try {
+    const data = await fetch((url += `&orderby=title&order=asc`), options);
+    const result = await data.json();
+
+    let blogsItems = result;
+
+    if (!isLoaded || selectedValues !== "AZ") {
+      sortCards(blogsItems);
+    }
+
+    for (let i = 0; i < blogsItems.length; i++) {
+      let blogTitle = blogsItems[i].title.rendered;
+      if (blogTitle.toLowerCase().includes(searchValue)) {
+        const featuredImage =
+          blogsItems[i]._embedded["wp:featuredmedia"][0].media_details.sizes
+            .medium.source_url;
+
+        const altText = blogsItems[i]._embedded["wp:featuredmedia"][0].alt_text;
+
+        const categoryId = blogsItems[i].categories[0];
+        const categoryName = await getCategoryName(categoryId);
+
+        travelBlogs.innerHTML += `
+        
+        <div class="cards">
+        <img class="cardsImg" src="${featuredImage}" alt="${altText}">
+      <a href="details.html?=id">
+      <h5 class="blogTitle">${blogTitle}</h5>
+      <div class="smallTags">
+      <p>${blogsItems[i].excerpt.rendered}</p>
+      <small>${categoryName}</small>
+      </div></a>
+      </div>
+      
+      `;
+      }
+    }
+    isLoaded = true;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const sortCards = (blogsItems) => {
+  if (selectedValues === "AZ") {
+    blogsItems.sort((a, b) => a.title.rendered.localeCompare(b.title.rendered));
+  } else if (selectedValues === "ZA") {
+    blogsItems.sort((a, b) => b.title.rendered.localeCompare(a.title.rendered));
+  }
+};
+
+const clearTravelBlogs = () => {
+  travelBlogs.innerHTML = "";
+};
+
+liAfrica.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=4`;
+    getBlogs();
+  }, 800);
+});
+
+liAmericas.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=6`;
+    getBlogs();
+  }, 800);
+});
+
+liAntarctica.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=5`;
+    getBlogs();
+  }, 800);
+});
+
+liAsia.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=3`;
+    getBlogs();
+  }, 800);
+});
+
+liEurope.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=7`;
+    getBlogs();
+  }, 800);
+});
+liOceania.addEventListener("click", async () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    url = `${url}&categories=8`;
+    getBlogs();
+  }, 800);
+});
+
+getBlogs();
+
+const loadMore = document.querySelector("#loadMore");
+loadMore.addEventListener("click", () => {
+  clearTravelBlogs();
+  setTimeout(() => {
+    blogsPostPerPage += 10;
+    url = `https://marvelous-jargon.flywheelsites.com/wp-json/wp/v2/posts?_embed&per_page=${blogsPostPerPage}`;
+    getBlogs();
+  }, 800);
+});
